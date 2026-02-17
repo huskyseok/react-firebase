2fa31dbb24ecdfec2689372c12d8781e40a3d190
huskyseok <leehyeonseok0812@gmail.com>
2026-02-17 18:56:29 +0900

    최초

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import AppRouter from './Router';
+import { authService } from '../firebase';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import '../App.css';
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setIsLoggedIn(true);
+      setUserObj(user.uid);
+    } else {
+      setIsLoggedIn(false);
+      setUserObj(null);
+    }
+    setInit(true);
+  });
+
+  return (
+    <>
+    {
+      init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}></AppRouter> : "회원정보 확인중..."
+    }
+    </>
+  );
+}
+
+export default App;
